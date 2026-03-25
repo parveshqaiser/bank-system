ed53ae2d9ba2fd820e1fcbc997f5d84485e67000
Parvesh Qaiser <ahmed75parvesh@gmail.com>
2026-03-25 22:24:55 +0530

    acc not in acc model mod

--- src/models/account.model.js
+++ src/models/account.model.js
@@ -25,7 +25,7 @@ const AccountSchema = new mongoose.Schema({
     },
     accountNumber : {
         type : String,
-        default : ()=> `ACC${Math.floor(1000000000 + Math.random() * 9000000000)}`,
+        default : ()=> `ACC${Math.floor(10000000 + Math.random() * 90000000)}`,
     },
     balance : {
         type : Number,
